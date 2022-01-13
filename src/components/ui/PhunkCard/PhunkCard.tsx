66a01cee59d42f151dcf8394396d52eee47e2bb6
Ishaan Rawat <ishaan@ishaanrawat.com>
2022-01-13 20:02:49 +0530

    image from cdn

--- src/components/ui/PhunkCard/PhunkCard.tsx
+++ src/components/ui/PhunkCard/PhunkCard.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { formatTokenId } from "../../../utils";
 
 interface PhunkCardProps {
   tokenId: string;
@@ -13,8 +14,14 @@ const PhunkCard: React.FC<PhunkCardProps> = ({ tokenId, image }) => {
   }, [tokenId]);
 
   return (
-    <div className="flex flex-col items-start p-2">
-      <img src={image} className="w-48 h-48 bg-gray-700" alt="" />
+    <div className="flex items-center p-2">
+      <img
+        src={`https://nll-v2-1-39luy.ondigitalocean.app/static/phunk${formatTokenId(
+          tokenId
+        )}.png`}
+        className="w-12 h-12 bg-gray-700 mr-4"
+        alt=""
+      />
       <span className="font-semibold">Phunk #{tokenId}</span>
     </div>
   );
